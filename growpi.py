e2cbb3c9a18593f427f2f861f8fb651317847964
Berend de Jonge <dejongeberend@gmail.com>
2019-02-02 15:12:20 +0100

    Code refactoring: in main loop added if/else lights on/off

--- growpi.py
+++ growpi.py
@@ -16,40 +16,43 @@ tempSensor = 4
 #waterPump = 5
 distSensor = 6
 
-#Heights
+# Heights
 potHeight = 12
 sensorHeight = 73
 
-displayInterval = 1 * 60 #How long should the display stay on?
-checkInterval = 10 * 60 # How long before loop starts again?
-lightThreshold = 10 # value at wich Light begins
+displayInterval = 1 * 60  # How long should the display stay on?
+checkInterval = 10 * 60  # How long before loop starts again?
+lightThreshold = 10  # value at wich Light begins
 
-mlSecond = 20 # How much mililiter the waterpump produces per second
-waterAmount = 500 # How much mililiter water should be given to the plants
+mlSecond = 20  # How much mililiter the waterpump produces per second
+waterAmount = 500  # How much mililiter water should be given to the plants
 
 
 def displayText():
-    setRGB(0,128,64) # background color led display
-    text = str(temp) + "C " + str(humidity) + "% " + str(measurePi() + "\n" + str(moist) + " " + moistResult + " " + str(lightValue) + " on")
-    
+    setRGB(0, 128, 64)  # background color led display
+    text = str(temp) + "C " + str(humidity) + "% " + str(measurePi() +
+                                                         "\n" + str(moist) + " " + moistResult + " " + str(lightValue) + " on")
+
     setText(text)
-    
+
     time.sleep(displayInterval)
-    setText("") 
-    setRGB(0,0,0)
-    
+    setText("")
+    setRGB(0, 0, 0)
+
+
 def calcPlantHeight():
-    return sensorHeight - potHeight - distValue 
+    return sensorHeight - potHeight - distValue
+
 
 def measurePi():
-        temp = os.popen("vcgencmd measure_temp").readline()
-        return (temp.replace("temp=","")[0:4])
-    
+    temp = os.popen("vcgencmd measure_temp").readline()
+    return (temp.replace("temp=", "")[0:4])
+
 # Write data to csv
 def appendCSV():
-    fields=['Time','Temperature','Humidity', 'MoistValue', 'MoistClass',
-            'LightValue', 'Lights', 'PiTemperature', 'Height', 'SonicDistance']
-    
+    fields = ['Time', 'Temperature', 'Humidity', 'MoistValue', 'MoistClass',
+              'LightValue', 'Lights', 'PiTemperature', 'Height', 'SonicDistance']
+
     with open(r'temp.csv', 'a') as f:
         writer = csv.DictWriter(f, fieldnames=fields)
         writer.writerow({'Time': currentTime,
@@ -61,31 +64,35 @@ def appendCSV():
                          'Lights': lightsOn,
                          'PiTemperature': (measurePi()),
                          'Height': (calcPlantHeight()),
-                         'SonicDistance' : distValue
+                         'SonicDistance': distValue
                          })
-        
+
+
 def printStatements():
     print(currentTime)
     if math.isnan(temp) == False and math.isnan(humidity) == False:
-        print("Temperature: {0:.02f}'C\nHumidity: {1:.02f}%".format(temp, humidity))
+        print("Temperature: {0:.02f}'C\nHumidity: {1:.02f}%".format(
+            temp, humidity))
     else:
         print("Couldn't get temperature/humidity sensor readings")
-        
+
     print('Moisture value: {0} ({1})'.format(moist, moistResult))
-    
+
     if lightsOn:
         print("Lights: {} (On)".format(lightValue))
     else:
         print("Lights: {} (Off)".format(lightValue))
-        
+
     print("Height: {} cm".format(calcPlantHeight()))
     print("Raspberry pi: {}'C\n".format(measurePi()))
 
+
 def waterPlants():
-    digitalwrite(waterPump,1)
-    time.sleep(waterAmount/mlSecond)
-    digitalwrite(waterPump,0)
+    digitalwrite(waterPump, 1)
+    time.sleep(waterAmount / mlSecond)
+    digitalwrite(waterPump, 0)
     print("Watering complete at: " + time.ctime())
+
 
 # Main Loop
 lightsOn = True
@@ -96,40 +103,51 @@ while True:
         lightValue = analogRead(lightSensor)
         distValue = ultrasonicRead(distSensor)
         moist = analogRead(moistSensor)
-        [temp,humidity] = dht(tempSensor,0)  
+        [temp, humidity] = dht(tempSensor, 0)
         currentTime = time.ctime()
-        
-        if lightValue <= lightThreshold:
-            lightsOn = False
-        else:
-            lightsOn = True
-            
+
         if 0 <= moist and moist < 300:
             moistResult = 'Dry'
-            digitalWrite(ledRed,1)
         elif 300 <= moist and moist < 600:
             moistResult = 'Moist'
-            digitalWrite(ledRed,0)
         else:
             moistResult = 'Wet'
-            digitalWrite(ledRed,0)
-            
-        printStatements()
-        appendCSV()
-        
-        # Not use display when it's dark
-        if lightsOn:
+
+        # Lights on
+        if lightValue > lightThreshold:
+            lightsOn = True
+
+            printStatements()
+            appendCSV()
+
+            # Turn on red LED when ground is dry, but only when lights are on
+            if moistResult == 'Dry':
+                digitalWrite(ledRed, 1)
+            else:
+                digitalWrite(ledRed, 0)
+
             displayText()
-            # Time to next check minus display sleep
+            # Time to next check minus display time
             time.sleep(checkInterval - displayInterval)
+
+        # Lights off
         else:
+            lightsOn = False
+
+            printStatements()
+            appendCSV()
+
+            # In case ground was dry, when lights where still on
+            digitalWrite(ledRed, 0)
+
             time.sleep(checkInterval)
 
     except KeyboardInterrupt:
-        digitalWrite(ledRed,0)
+        digitalWrite(ledRed, 0)
         setText("")
-        setRGB(0,0,0)
+        setRGB(0, 0, 0)
         print("Leds and RGB shutdown safely")
         break
     except IOError:
-        print ("Error")
+        print("Error")
+
